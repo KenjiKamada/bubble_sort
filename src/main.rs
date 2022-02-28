62946ebe423c5970baf456d2ac3cb23fc0a05015
Kenji Kamada <kamadak@kawai.co.jp>
2022-02-28 14:32:01 +0900

    bubble_sort()実装

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,54 @@ use rand::Rng;
 
 fn main() {
 
-    let a = 1000;
-
-    let mut x: [i32; 101]=[0;101];
-    let mut y: [i32; 100]=[0;100];
+    let mut x: [i32; 101]=[0;101]; //乱数
+    let mut y: [i32; 100]=[0;100]; // ソート結果
 
     // -1000 以上 1000 以下のランダムな整数を100個用意する。
     for n in 1..x.len(){
-        x[n] = rand::thread_rng().gen_range(-(a+1), a+1);
+        x[n] = rand::thread_rng().gen_range(-(1000+1), 1000+1);
     }
-
+    bubble_sort(&mut x, &mut y);
     
     //let mut tmp: [i32;2]=[0;2];
-    let mut tmp = [0,0];
-    
-    
+  //  let mut tmp = [0,0];
+   
+    // バブルソート
+    // for n in 1.. x.len(){
+    //     for m in 2.. x.len(){
+    //         tmp[0]=x[m-1];  tmp[1]=x[m];
+    //                if x[m] < x[m-1]{
+    //             x[m-1] = tmp[1];
+    //             x[m] = tmp[0];
+    //         }
+    //     } 
+    // }
+
+ 
+
+     // copy x -> y
+    // for n in 0.. y.len(){
+    //     y[n] = x[n+1];
+    //   }
+
+    // 確認  yにソート結果
+    // 全部"True！"なら良い。
+    for n in 0.. y.len()-1{
+      //  println!("n={}, x={}, y={}", n,x[n], y[n]);
+
+        if y[n] <= y[n+1] {
+            println!("True!")
+        }else{
+            println!("False...")
+        }
+    }
+
+}
+fn bubble_sort(x: &mut [i32], y: &mut [i32]){
+
+  //  println!("x={}",x[10]);
+  let mut tmp = [0,0];
+
     // バブルソート
     for n in 1.. x.len(){
         for m in 2.. x.len(){
@@ -30,14 +63,8 @@ fn main() {
 
     // copy x -> y
     for n in 0.. y.len(){
-        y[n] = x[n+1];
-      }
-
-       // 確認
-    for n in 0.. y.len(){
-        println!("n={}, x={}, y={}", n,x[n], y[n]);
+    y[n] = x[n+1];
     }
-
 }
 
 
